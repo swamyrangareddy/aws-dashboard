3e979c0ca43d7cfcbe83b2b0d40a4ee030fe8c70
Swamyrangareddy Muthumula <127617789+swamyrangareddy@users.noreply.github.com>
2024-11-20 16:37:48 +0530

    Update aws_stripe.py

--- aws_stripe.py
+++ aws_stripe.py
@@ -21,12 +21,12 @@ class Dashboard:
         self.s3_config = {
             "bucket_name": "my-s3-dashboard",
             "files": {
-                "revenue": "my-s3-dashboard/KPI_Revenue_total_counts.csv",
-                "customers": "my-s3-dashboard/customers_6months..csv",
-                "subscriptions": "my-s3-dashboard/subscriptions_6months.csv",
-                "payment": "my-s3-dashboard/payments_outcome_data.csv",
-                "financial": "my-s3-dashboard/financial.csv",
-                "customer_metadata" :"my-s3-dashboard/customers_metadata.csv"
+                "revenue": "KPI_Revenue_total_counts.csv", 
+                "customers": "customers_6months.csv", 
+                "subscriptions": "subscriptions_6months.csv", 
+                "payment": "payments_outcome_data.csv",
+                "financial": "financial.csv", 
+                "customer_metadata": "customers_metadata.csv"  
             }}
     
     def load_data_from_s3(self, file_key):
